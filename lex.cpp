988e115a64b20a4c00a46cb0fa1bc2ec6554e520
Nishank-25 <nishankpatil25@gmail.com>
2023-09-01 22:22:51 +0530

    Task 1

---
+++ lex.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+   
+   // Task 1.1 : Read from the file
+
+   std::cout<<"hello\n";
+
+   // Task 1.2 : Scan the source and save it
+   // Task 1.3 : Tokenize
+   // Task 1.4 : Print the program as it was in the file
+
+}
